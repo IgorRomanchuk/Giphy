b6c6bc0dde6ad8c4380bf89e5bc59da90cb89490
Igor Romanchuk <defhn900@gmail.com>
2024-05-01 00:06:58 +0300

    minor fix

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom'
 
 import Header from '../components/header'
-import s from './layout.module.css'
+import s from './layout.module.scss'
 
 const Layout = () => {
   return (
