3fb79a2f1b9574d4beb2777a09496f70038113bb
Igor Romanchuk <defhn900@gmail.com>
2024-05-04 21:23:16 +0300

    added GifsContainer and refactoring in searchGifs and home pages

---
+++ src/components/gifsContainer/index.tsx
@@ -0,0 +1,41 @@
+import InfiniteScroll from 'react-infinite-scroll-component'
+import Masonry from 'react-responsive-masonry'
+
+import CardGif from '../../components/cardGif'
+import { Gif } from '../../types/Gif'
+import getRandomInt from '../../utils/getRandomInt'
+const GifsContainer = ({ gifsArray, fetchData }: any) => {
+  // const offset = useSelector((state: any) => state.trendingGifs.offset)
+  // const fetchData = () => {
+  //   dispatch(fetchTrendingGifs(offset))
+  // }
+
+  return (
+    <>
+      {gifsArray && (
+        <div>
+          <InfiniteScroll
+            dataLength={gifsArray.length}
+            next={fetchData}
+            hasMore={true}
+            loader={<h4>Loading...</h4>}
+            endMessage={
+              <p style={{ textAlign: 'center' }}>
+                <b>Yay! You have seen it all</b>
+              </p>
+            }
+          >
+            <Masonry columnsCount={4} gutter="10px">
+              {gifsArray.map((image: Gif, i: number) => {
+                const num = getRandomInt(4)
+                return <CardGif key={i} index={num} image={image} />
+              })}
+            </Masonry>
+          </InfiniteScroll>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default GifsContainer
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,8 @@
-import InfiniteScroll from 'react-infinite-scroll-component'
 import { useDispatch, useSelector } from 'react-redux'
-import Masonry from 'react-responsive-masonry'
 
-import CardGif from '../../components/cardGif'
+import GifsContainer from '../../components/gifsContainer'
 import { fetchTrendingGifs } from '../../store/gifsSlice'
-import { Gif } from '../../types/Gif'
-import getRandomInt from '../../utils/getRandomInt'
+
 const Home = () => {
   const dispatch: any = useDispatch()
   const trendingGifs = useSelector(
@@ -18,28 +15,7 @@ const Home = () => {
 
   return (
     <>
-      {trendingGifs && (
-        <div>
-          <InfiniteScroll
-            dataLength={trendingGifs.length}
-            next={fetchData}
-            hasMore={true}
-            loader={<h4>Loading...</h4>}
-            endMessage={
-              <p style={{ textAlign: 'center' }}>
-                <b>Yay! You have seen it all</b>
-              </p>
-            }
-          >
-            <Masonry columnsCount={4} gutter="10px">
-              {trendingGifs.map((image: Gif, i: number) => {
-                const num = getRandomInt(4)
-                return <CardGif key={i} index={num} image={image} />
-              })}
-            </Masonry>
-          </InfiniteScroll>
-        </div>
-      )}
+      <GifsContainer fetchData={fetchData} gifsArray={trendingGifs} />
     </>
   )
 }
