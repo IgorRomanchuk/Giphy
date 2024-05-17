3f817c128cd6f733a401ea80c6f0e4d8344d91ec
Igor Romanchuk <defhn900@gmail.com>
2024-05-18 00:39:38 +0300

    minor fix for notfound page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,22 +1,10 @@
-import { useNavigate } from 'react-router-dom'
-
 const NotFound = () => {
-  const navigate = useNavigate()
-  // const dispatch = useAppDispatch()
   return (
     <div>
-      <h3>
+      <h3 style={{ color: 'white', paddingTop: '20px' }}>
         Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve
         mistyped the URL? Be sure to check your spelling.
       </h3>
-      <button
-        onClick={() => {
-          navigate('/')
-          // dispatch(setActiveNav('home'))
-        }}
-      >
-        Go to home
-      </button>
     </div>
   )
 }
