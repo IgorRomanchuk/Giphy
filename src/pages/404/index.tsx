5a585de07eac5b5fb4cc3d05be49d631b773528f
Igor Romanchuk <defhn900@gmail.com>
2024-06-01 23:57:58 +0300

    fixed bug in notfound page, bug in adaptive

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,6 +1,8 @@
+import s from './notFound.module.scss'
+
 const NotFound = () => {
   return (
-    <div>
+    <div className={s.title}>
       <h3 style={{ color: 'white', paddingTop: '20px' }}>
         Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve
         mistyped the URL? Be sure to check your spelling.
