172e7309744bf01842e71c1685a5dd267d0ce11d
Igor Romanchuk <defhn900@gmail.com>
2025-04-29 17:59:34 +0300

    craco added

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
+import GifsContainer from '@components/gifsContainer'
 import { useEffect } from 'react'
 
-import GifsContainer from '../../components/gifsContainer'
 import { useAppDispatch } from '../../hooks/useAppDispatch'
 import { useAppSelector } from '../../hooks/useAppSelector'
 import { fetchTrendingGifs } from '../../store/gifsSlice'
