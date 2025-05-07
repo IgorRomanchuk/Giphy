18a36895407ca85826163972ef68e68b8ca03ea6
Igor Romanchuk <defhn900@gmail.com>
2025-05-07 17:09:31 +0300

    gifs api, remove redudant base url

--- src/shared/api/gifs.api.ts
+++ src/shared/api/gifs.api.ts
@@ -6,16 +6,16 @@ import { ParamsDto } from '@shared/models/params.dto'
 export const GifsApi = {
   getTrendingGifs: async (params: Partial<ParamsDto>): Promise<GifSchema[]> =>
     (
-      await api.get(`${BASE_URL}/gifs/trending`, {
+      await api.get(`/gifs/trending`, {
         params,
       })
     ).data.data,
   getGifsByValue: async (params: Partial<ParamsDto>): Promise<GifSchema[]> =>
     (
-      await api.get(`${BASE_URL}/gifs/search`, {
+      await api.get(`/gifs/search`, {
         params,
       })
     ).data.data,
   getGifById: async (gifId: string): Promise<GifSchema> =>
-    (await api.get(`${BASE_URL}/gifs/${gifId}`)).data.data,
+    (await api.get(`/gifs/${gifId}`)).data.data,
 }
